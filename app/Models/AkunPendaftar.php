3520e03f3d72d933394855a0430daa5c0e6a224b
fakhryfernanda <mff023@gmail.com>
2022-12-29 04:49:35 +0700

    (+) Boot function

--- app/Models/AkunPendaftar.php
+++ app/Models/AkunPendaftar.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class AkunPendaftar extends Model
+class AkunPendaftar extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // fungsi dijalankan ketika membuat model
+        static::creating(function ($user) {
+            // hashing password
+            $hash = Hash::make($user->password);
+            $user->password = $hash;
+            // $user->password = bcrypt($user->password);
+        });
+
+        self::updating(function ($user) {
+            // memeriksa apakah password berubah
+            if ($user->isDirty(["password"])) {
+                $hash = Hash::make($user->password);
+                $user->password = $hash;
+            }
+        });
+    }
 }
