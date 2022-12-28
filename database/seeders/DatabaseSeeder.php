386d48ef0f43dc45deac9419e768cebaada763ac
fakhryfernanda <mff023@gmail.com>
2022-12-29 04:50:05 +0700

    Buat satu dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AkunPendaftar;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        AkunPendaftar::query()->create([
+            "nik" => "3275092303000022",
+            "nomor_pendaftaran" => "12345678",
+            "email" => "mff023@gmail.com",
+            "password" => "1234",
+        ]);
     }
 }
