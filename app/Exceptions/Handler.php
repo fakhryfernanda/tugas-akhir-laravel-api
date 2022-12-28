720e687088672ac899009853c463e85eb6ba257f
fakhryfernanda <mff023@gmail.com>
2022-12-29 04:48:23 +0700

    Handler kalo tokennya salah

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Psr\Log\LogLevel;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +46,17 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        // kalau tokennya salah
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->is("api/*")) {
+                return response()->json([
+                    "status" => false,
+                    "message" => "unauthorized",
+                    "data" => null
+                ]);
+            }
         });
     }
 }
