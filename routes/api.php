b19a124097c825ac34479b13d4e33ff3daae836f
fakhryfernanda <mff023@gmail.com>
2022-12-29 04:50:42 +0700

    (+) route untuk login dan getUser

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post("/login", [AuthController::class, 'login'])->name('login');
+Route::get("/me", [AuthController::class, 'getUser'])->middleware('auth:sanctum');
