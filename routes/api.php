105480b6523543d30fb70e29543782b4e15562c7
fakhryfernanda <mff023@gmail.com>
2022-12-29 16:27:04 +0700

    (+) route api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PendaftarController;
 
 Route::post("/login", [AuthController::class, 'login'])->name('login');
 Route::get("/me", [AuthController::class, 'getUser'])->middleware('auth:sanctum');
+
+Route::prefix('pendaftar')->group(function(){
+    Route::get("/all", [PendaftarController::class, "index"]);
+    Route::get("/detail/{id}", [PendaftarController::class, "detail"]);
+    Route::post("/add", [PendaftarController::class, "store"]);
+    Route::put("/edit/{id}", [PendaftarController::class, "edit"]);
+});
