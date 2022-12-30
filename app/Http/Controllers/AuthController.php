a924889cd00f11eefb96982049f1ae29d5da2198
fakhryfernanda <mff023@gmail.com>
2022-12-30 13:50:40 +0700

    (+) api register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,4 +62,66 @@ class AuthController extends Controller
             "data" => $user
         ]);
     }
+
+    public function register(Request $request)
+    {
+        $payload = $request->all();
+
+        $columns = [
+            'nik',
+            'email',
+            'password'
+        ];
+
+        foreach($columns as $col) {
+            if (!isset($payload[$col])) {
+                $message = "{$col} tidak boleh kosong";
+                return response()->json([
+                    "status" => false,
+                    "message" => $message,
+                    "data" => null
+                ]);
+            }
+        }
+
+
+        $list_nik = (array) AkunPendaftar::all()->pluck('nik');
+        $key = array_keys($list_nik)[0];
+        if (in_array($payload['nik'], $list_nik[$key])) {
+            return response()->json([
+                "status" => false,
+                "message" => "NIK sudah terdaftar",
+                "data" => null
+            ]);
+        }
+
+        $list_email = (array) AkunPendaftar::all()->pluck('email');
+        $key = array_keys($list_email)[0];
+        if (in_array($payload['email'], $list_email[$key])) {
+            return response()->json([
+                "status" => false,
+                "message" => "Email sudah terdaftar",
+                "data" => null
+            ]);
+        }
+
+        $list_nomor_pendaftaran = (array) AkunPendaftar::all()->pluck('nomor_pendaftaran');
+        $key = array_keys($list_nomor_pendaftaran)[0];
+        while (true) {
+            $nomor_pendaftaran = mt_rand(11111111,99999999);
+            if (!in_array($nomor_pendaftaran, $list_nomor_pendaftaran[$key])) {
+                break;
+            }
+        }
+
+        $payload['password'] = bcrypt($payload['password']);
+        $payload['nomor_pendaftaran'] = $nomor_pendaftaran;
+
+        $akun_pendaftar = AkunPendaftar::create($payload);
+        return response()->json([
+            "status" => true,
+            "message" => "Akun berhasil dibuat. Simpan baik-baik nomor pendaftaranmu.",
+            "data" => $akun_pendaftar->nomor_pendaftaran
+        ]);
+    }
 }
