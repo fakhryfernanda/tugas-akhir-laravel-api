08989c8d8bc1238ffcf981c85fc90a01e0b6696b
fakhryfernanda <mff023@gmail.com>
2022-12-30 13:49:15 +0700

    (+) route register

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\PendaftarController;
 
 Route::post("/login", [AuthController::class, 'login'])->name('login');
 Route::get("/me", [AuthController::class, 'getUser'])->middleware('auth:sanctum');
+Route::post("/register", [AuthController::class, 'register']);
+
 
 Route::prefix('pendaftar')->group(function(){
     Route::get("/all", [PendaftarController::class, "index"]);
